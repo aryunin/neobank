f6699d0e1f08ee40770cb28195130084deb4dce8
aryunin <im.aryunin@gmail.com>
2023-07-31 16:59:06 +0300

    ScoringDataDTO

---
+++ conveyor/src/main/java/com/aryunin/conveyor/dto/EmploymentDTO.java
@@ -0,0 +1,35 @@
+package com.aryunin.conveyor.dto;
+
+import com.aryunin.conveyor.util.EmploymentStatus;
+import com.aryunin.conveyor.util.MoneySerializer;
+import com.aryunin.conveyor.util.Position;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.Builder;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+public class EmploymentDTO {
+    @NotNull
+    private EmploymentStatus employmentStatus;
+    @NotNull
+    @Pattern(regexp = "\\d{20}")
+    private String employerINN;
+    @JsonSerialize(using = MoneySerializer.class)
+    @NotNull
+    @Min(value = 0, message = "negative salary")
+    private BigDecimal salary;
+    @NotNull
+    private Position position;
+    @NotNull
+    @Min(value = 0, message = "negative total work experience")
+    private Integer workExperienceTotal;
+    @NotNull
+    @Min(value = 0, message = "negative work experience")
+    private Integer workExperienceCurrent;
+}
---
+++ conveyor/src/main/java/com/aryunin/conveyor/util/Position.java
@@ -0,0 +1,7 @@
+package com.aryunin.conveyor.util;
+
+public enum Position {
+    WORKER,
+    MANAGER,
+    DIRECTOR
+}
