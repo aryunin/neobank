2fa96a30ed28c9b3c8c127a94a58dafa273a88f2
aryunin <im.aryunin@gmail.com>
2023-07-31 20:34:41 +0300

    CalculationController

---
+++ conveyor/src/main/java/com/aryunin/conveyor/service/CalculationService.java
@@ -0,0 +1,10 @@
+package com.aryunin.conveyor.service;
+
+import com.aryunin.conveyor.dto.CreditDTO;
+import com.aryunin.conveyor.dto.ScoringDataDTO;
+
+public class CalculationService {
+    public CreditDTO getCredit(ScoringDataDTO request) {
+        return null;
+    }
+}
