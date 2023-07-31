bf805f55aa10ce66bba12f68b0ebc373e93ad04d
aryunin <im.aryunin@gmail.com>
2023-07-31 16:59:41 +0300

    CreditDTO

---
+++ conveyor/src/main/java/com/aryunin/conveyor/dto/PaymentScheduleElement.java
@@ -0,0 +1,20 @@
+package com.aryunin.conveyor.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Builder;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Data
+@Builder
+public class PaymentScheduleElement {
+    private Integer number;
+    @JsonFormat(pattern="yyyy-MM-dd")
+    private LocalDate date;
+    private BigDecimal totalPayment;
+    private BigDecimal interestPayment;
+    private BigDecimal debtPayment;
+    private BigDecimal remainingDept;
+}
