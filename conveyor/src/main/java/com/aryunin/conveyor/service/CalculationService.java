0516fa3ea97665a857655164ee69e31691ceea2c
aryunin <im.aryunin@gmail.com>
2023-07-31 20:45:32 +0300

    CalculationController

--- conveyor/src/main/java/com/aryunin/conveyor/service/CalculationService.java
+++ conveyor/src/main/java/com/aryunin/conveyor/service/CalculationService.java
@@ -2,7 +2,9 @@ package com.aryunin.conveyor.service;
 
 import com.aryunin.conveyor.dto.CreditDTO;
 import com.aryunin.conveyor.dto.ScoringDataDTO;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CalculationService {
     public CreditDTO getCredit(ScoringDataDTO request) {
         return null;
