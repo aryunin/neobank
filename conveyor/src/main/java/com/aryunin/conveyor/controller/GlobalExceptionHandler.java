60b131803ca160e7e47c33dca5eb758c8699396d
aryunin <im.aryunin@gmail.com>
2023-08-01 11:28:12 +0300

    GlobalHandler moved to controller/GlobalExceptionHandler

---
+++ conveyor/src/main/java/com/aryunin/conveyor/controller/GlobalExceptionHandler.java
@@ -0,0 +1,34 @@
+package com.aryunin.conveyor.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    private Map<String, String> formatFieldErrors(List<FieldError> errors) {
+        return errors.stream().collect(
+                Collectors.toMap(
+                        FieldError::getField,
+                        e -> (e.getDefaultMessage() == null) ? "" : e.getDefaultMessage()
+                )
+        );
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ProblemDetail handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
+        ProblemDetail pd = ex.getBody();
+        pd.setProperty("fields", formatFieldErrors(ex.getFieldErrors()));
+        return pd;
+    }
+}
