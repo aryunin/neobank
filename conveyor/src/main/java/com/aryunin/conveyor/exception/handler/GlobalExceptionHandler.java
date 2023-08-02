712fa66263a661089105c77c25557920087ce582
aryunin <im.aryunin@gmail.com>
2023-08-02 14:28:10 +0300

    FieldErrorsResponse & CommonExceptionHandler

--- conveyor/src/main/java/com/aryunin/conveyor/exception/handler/GlobalExceptionHandler.java
+++ conveyor/src/main/java/com/aryunin/conveyor/exception/handler/GlobalExceptionHandler.java
@@ -1,38 +1,27 @@
-package com.aryunin.conveyor.controller;
+package com.aryunin.conveyor.exception.handler;
 
+import com.aryunin.conveyor.exception.FieldErrorsResponse;
 import com.aryunin.conveyor.exception.ScoringFailedException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
-
-// TODO common error response (for ex 404)
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    private Map<String, String> formatFieldErrors(List<FieldError> errors) {
-        return errors.stream().collect(
-                Collectors.toMap(
-                        FieldError::getField,
-                        e -> (e.getDefaultMessage() == null) ? "" : e.getDefaultMessage()
-                )
-        );
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ProblemDetail handleNoHandlerFoundException(NoHandlerFoundException ex) {
+        return ex.getBody();
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ProblemDetail handleValidationException(
-            MethodArgumentNotValidException ex) {
-        ProblemDetail pd = ex.getBody();
-        pd.setProperty("fields", formatFieldErrors(ex.getFieldErrors()));
-        return pd;
+    public ProblemDetail handleValidationException(MethodArgumentNotValidException ex) {
+        return new FieldErrorsResponse(ex, ex.getFieldErrors()).getBody();
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
