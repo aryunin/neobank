cce575d4665caf12b44bfeb5ecddfa03f4cc4387
aryunin <im.aryunin@gmail.com>
2023-08-02 16:09:35 +0300

    controllers logging

--- conveyor/src/main/java/com/aryunin/conveyor/controller/OffersController.java
+++ conveyor/src/main/java/com/aryunin/conveyor/controller/OffersController.java
@@ -4,19 +4,26 @@ import com.aryunin.conveyor.dto.LoanApplicationRequestDTO;
 import com.aryunin.conveyor.dto.LoanOfferDTO;
 import com.aryunin.conveyor.service.offer.OffersService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/offers")
 @RequiredArgsConstructor
+@Slf4j
 public class OffersController {
     private final OffersService service;
 
     @PostMapping
-    @ResponseBody
     public List<LoanOfferDTO> getOffers(@RequestBody LoanApplicationRequestDTO request) {
-        return service.getOffers(request);
+        log.info("processing offers for " + request.getEmail());
+        var result = service.getOffers(request);
+        log.info("offers for " + request.getEmail() + " processed");
+        return result;
     }
 }
