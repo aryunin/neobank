3fa60041dc9d42a5d9bb3325fb630fe6dca753df
aryunin <im.aryunin@gmail.com>
2023-08-02 22:25:06 +0300

    CalculationController documentation

--- conveyor/src/main/java/com/aryunin/conveyor/controller/CalculationController.java
+++ conveyor/src/main/java/com/aryunin/conveyor/controller/CalculationController.java
@@ -3,6 +3,8 @@ package com.aryunin.conveyor.controller;
 import com.aryunin.conveyor.dto.CreditDTO;
 import com.aryunin.conveyor.dto.ScoringDataDTO;
 import com.aryunin.conveyor.service.calculation.CalculationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -12,9 +14,14 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/calculation")
 @RequiredArgsConstructor
 @Slf4j
+@Tag(name = "Calculation", description = "Loan management APIs")
 public class CalculationController {
     private final CalculationService service;
 
+    @Operation(
+            summary = "Get loan",
+            description = "Validate data, score it and get available loan as response"
+    )
     @PostMapping
     public CreditDTO getCredit(@RequestBody @Valid ScoringDataDTO request) {
         log.info("processing credit for " + request.getLastName() + " " + request.getFirstName());
